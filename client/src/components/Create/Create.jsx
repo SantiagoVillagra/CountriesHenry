b7911d159fb1ce1c82f2ef7e22b65f7efe129c12
SantiagoVillagra <sanmavillagra@gmail.com>
2024-07-02 00:41:13 -0300

    front 98%

--- client/src/components/Create/Create.jsx
+++ client/src/components/Create/Create.jsx
@@ -1,8 +1,254 @@
+// import { useState } from "react"
+// import { useSelector } from "react-redux"
+// import axios from "axios"
+// import styles from "./form.module.css"
+// import validations from "./validations"
+
+// export default function Form() {
+
+//     const formDefault = {
+//         name: "",
+//         dificulty: "",
+//         duration: 1,
+//         season: "",
+//         country: ""
+//     }
+
+//     const [form, setForm] = useState(formDefault)
+
+//     const selectCountriesDefault = []
+
+//     const [selectCountries, setSelectCountries] = useState(selectCountriesDefault)
+
+//     const allCountries = useSelector((state) => state.allCountries)
+
+//     const [errors, setErrors] = useState({})
+
+//     const orderedCountries = [...allCountries].sort((a, b) => {
+//         return a.name.localeCompare(b.name)
+//     })
+
+//     const handleChange = (event) => {
+//         setForm({...form, [event.target.name]: event.target.value})
+
+//         validations(event.target.name, value, errors, setErrors, {...form, [event.target.name]: event.target.value})+
+     
+//         consolelog(errors)
+//     }
+
+//     const handleCountries = (event, index) => {
+//         const newSelect = [...selectCountries]
+//         newSelect[index].country = event.target.value
+//         setSelectCountries(newSelect)
+
+//         validations("countries", errors, setErrors, newSelect)
+//     }
+
+//     const handleSubmit = (event) => {
+//         event.preventDefault()
+
+//         const errores = Object.values(errors).filter(error => error.length)// metodo que devuelve las propiedades de un objeto, se hace filtrado para ver si hay error
+
+//         if (errores.length) {
+//             window.alert("Revisa que todos los campos hayan sido completados correctamente.")
+//             return
+//         }
+
+//         const addedCountries = selectCountries.map(country => country.country)
+
+//         const body = {...form, id: addedCountries.concat(form.country), difficulty: parseInt(form.dificulty), duration: parseInt(form.duration)}
+
+
+//         console.log(body);
+//         axios.post("http://localhost:3001/activities/", body)
+//         .then(response => window.alert("Actividad creada"))
+//         setForm(formDefault)
+//         setSelectCountries(selectCountriesDefault)
+//     }
+
+//     const addCountry = () => {
+//         if (form.country === "" || form.country === "Selecciona un country--") {
+//             setErrors({...errors, countries: "Debes completar el campo anterior antes de agregar un nuevo país"})
+//             return
+//         }
+        
+//         if (selectCountries.length && selectCountries[selectCountries.length-1].country === "") {
+//             setErrors({...errors, countries: "Debes completar el campo anterior antes de agregar un nuevo país"})
+//             return
+//         } else {
+//             setErrors({...errors, countries:""})
+//         }
+//         if (errors.countries) return
+        
+//         const newSelectCountries = [...selectCountries, {country: ""}]
+//         setSelectCountries(newSelectCountries)
+//     }
+
+//     const quitarCountry = () => {
+//         const oneLess = selectCountries.slice(0, -1)
+//         setSelectCountries(oneLess)        
+//     }
+
+//     return (
+//         <div className={styles.Form}>
+//             <form onSubmit={handleSubmit}>
+
+//                 <div>
+//                     <label htmlFor="name">Actividad: </label>
+//                     <input 
+//                         type="text"
+//                         name="name"
+//                         value={form.name}
+//                         onChange={handleChange}
+//                     />
+//                     <br />
+//                     <span>{errors.name}</span>
+//                 </div>
+
+//                 <div>
+//                     <label htmlFor="dificulty">Dificultad: </label>
+//                     <select name="dificulty" id="dificulty" value={form.dificulty} onChange={handleChange}>
+//                         <option value="seleccionar">Selecciona nivel de dificultad</option>
+//                         <option value={1}>{"1 (uno)"}</option>
+//                         <option value={2}>{"2 (dos)"}</option>
+//                         <option value={3}>{"3 (tres)"}</option>
+//                         <option value={4}>{"4 (cuatro)"}</option>
+//                         <option value={5}>{"5 (cinco)"}</option>
+//                     </select>
+//                     <br />
+//                     <span>{errors.dificulty}</span>
+//                 </div>
+
+//                 <div className={styles.duration}>
+//                     <label htmlFor="duration">Duracion: </label>
+//                     <input 
+//                         type="range"
+//                         name="duration"
+//                         min={1}
+//                         max={160}
+//                         value={form.duration}
+//                         onChange={handleChange}
+//                     />
+//                     {form.duration}min.
+//                     <br />
+//                     <span>{errors.duration}</span>
+//                 </div>
+
+//                 <div>
+//                     <label htmlFor="season">Temporada: </label>
+//                     <div className={styles.radios}>
+//                         <input
+//                             type="radio"
+//                             name="season"
+//                             id="summer"
+//                             value="summer"
+//                             checked={form.season === "summer"}
+//                             onChange={handleChange}
+//                             />
+//                         <label htmlFor="summer">
+//                         Summer
+//                         </label>
+//                         <br />
+//                         <span>{errors.season}</span>
+//                     </div>
+
+//                     <div className={styles.radios}>
+//                         <input 
+//                             type="radio" 
+//                             name="season"
+//                             id="autumn"
+//                             value="autumn"
+//                             checked={form.season === "autumn"}
+//                             onChange={handleChange}
+//                             />
+//                         <label htmlFor="autumn">
+//                             Autumn
+//                         </label>
+//                     </div>
+
+//                     <div className={styles.radios}>
+//                         <input 
+//                             type="radio" 
+//                             name="season" 
+//                             id="winter" 
+//                             value="winter" 
+//                             checked={form.season === "winter"} 
+//                             onChange={handleChange}
+//                         />
+//                         <label htmlFor="winter">
+//                             Winter
+//                         </label>
+//                     </div>
+
+//                     <div className={styles.radios}>
+//                         <input 
+//                             type="radio" 
+//                             name="season" 
+//                             id="spring" 
+//                             value="spring" 
+//                             checked={form.season === "spring"} 
+//                             onChange={handleChange}
+//                         />
+//                         <label htmlFor="spring">
+//                             Spring
+//                         </label>
+//                     </div>
+
+//                     <br />
+//                     <span>{errors.season}</span>
+//                 </div>
+
+//                 <div>
+//     <label htmlFor="country">Selecciona un país: </label>
+//     <select name="country" id="country" value={form.country} onChange={handleChange}>
+//         <option value="Selecciona un país--">Selecciona un país--</option>
+//         {orderedCountries?.map((country, index) => (
+//             <option key={country.id} value={country.id}>{country.name}</option>
+//         ))}
+//     </select>
+//     <br />
+//     <span>{errors.country}</span>
+// </div>
+
+//                 <div>
+//                     {selectCountries?.map((select, index) => {
+//                         return (
+//                             <div key={index}>
+//                                 <label htmlFor={index}>Selecciona un country: </label>
+//                                 <select name={index} id={index} value={selectCountries[index].country} onChange={(event) => handleCountries(event, index)}>
+//                                     <option value="">Selecciona un country--</option>
+//                                     {orderedCountries.map((country, index) => {
+//                                         return (
+//                                             <option value={`${country.id}`}>{`${country.name}`}</option>
+//                                         )
+//                                     })
+//                                     }
+//                                 </select>
+//                             </div>
+//                         )
+//                     })}
+//                     <span>{errors.countries}</span>
+
+//                 </div>
+
+//                 <div className={styles.addRemove}>
+//                     <button type="button" onClick={addCountry}>Add Country</button>
+//                     <button type="button" onClick={quitarCountry}>Remove Country</button>
+//                 </div>
+
+//                 <button type="submit">Submit</button>
+
+//             </form>
+//         </div>
+//     )
+// }
+
 import { useState } from "react"
 import { useSelector } from "react-redux"
 import axios from "axios"
 import styles from "./form.module.css"
 import validations from "./validations"
+import { validateForm } from "./validations"
 
 export default function Form() {
 
@@ -29,11 +275,10 @@ export default function Form() {
     })
 
     const handleChange = (event) => {
-        setForm({...form, [event.target.name]: event.target.value})
+        const { name, value } = event.target;
+        setForm({...form, [name]: value})
 
-        validations(event.target.name, value, errors, setErrors, {...form, [event.target.name]: event.target.value})+
-     
-        consolelog(errors)
+        validations(name, errors, setErrors, {...form, [name]: value})
     }
 
     const handleCountries = (event, index) => {
@@ -44,30 +289,34 @@ export default function Form() {
         validations("countries", errors, setErrors, newSelect)
     }
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
+const handleSubmit = (event) => {
+        event.preventDefault();
 
-        const errores = Object.values(errors).filter(error => error.length)// metodo que devuelve las propiedades de un objeto, se hace filtrado para ver si hay error
+        const newErrors = validateForm(form, selectCountries, setErrors);
+        const hasErrors = Object.keys(newErrors).length > 0;
 
-        if (errores.length) {
-            window.alert("Revisa que todos los campos hayan sido completados correctamente.")
-            return
+        if (hasErrors) {
+            window.alert("Revisa que todos los campos hayan sido completados correctamente.");
+            setForm(formDefault); // Reiniciar el estado del formulario
+            setSelectCountries([]); // Vaciar la lista de países seleccionados
+            return;
         }
 
-        const addedCountries = selectCountries.map(country => country.country)
+        const addedCountries = selectCountries.map((country) => country.country);
+        const body = {...form, id: addedCountries.concat(form.country), difficulty: parseInt(form.dificulty), duration: parseInt(form.duration)};
 
-        const body = {...form, id: addedCountries.concat(form.country), difficulty: parseInt(form.dificulty), duration: parseInt(form.duration)}
-
-
-        console.log(body);
         axios.post("http://localhost:3001/activities/", body)
-        .then(response => window.alert("Actividad creada"))
-        setForm(formDefault)
-        setSelectCountries(selectCountriesDefault)
-    }
+            .then((response) => window.alert("Actividad creada"))
+            .catch((error) => console.error("Error al crear la actividad:", error));
+
+        setForm(formDefault);
+        setSelectCountries([]);
+    };
+
+
 
     const addCountry = () => {
-        if (form.country === "" || form.country === "Selecciona un country--") {
+        if (form.country === "" || form.country === "Selecciona un país--") {
             setErrors({...errors, countries: "Debes completar el campo anterior antes de agregar un nuevo país"})
             return
         }
@@ -136,6 +385,7 @@ export default function Form() {
 
                 <div>
                     <label htmlFor="season">Temporada: </label>
+                    <span>{errors.season}</span>
                     <div className={styles.radios}>
                         <input
                             type="radio"
@@ -149,7 +399,7 @@ export default function Form() {
                         Summer
                         </label>
                         <br />
-                        <span>{errors.season}</span>
+                       
                     </div>
 
                     <div className={styles.radios}>
@@ -199,16 +449,16 @@ export default function Form() {
                 </div>
 
                 <div>
-    <label htmlFor="country">Selecciona un país: </label>
-    <select name="country" id="country" value={form.country} onChange={handleChange}>
-        <option value="Selecciona un país--">Selecciona un país--</option>
-        {orderedCountries?.map((country, index) => (
-            <option key={country.id} value={country.id}>{country.name}</option>
-        ))}
-    </select>
-    <br />
-    <span>{errors.country}</span>
-</div>
+                    <label htmlFor="country">Selecciona un país: </label>
+                    <select name="country" id="country" value={form.country} onChange={handleChange}>
+                        <option value="Selecciona un país--">Selecciona un país--</option>
+                        {orderedCountries?.map((country, index) => (
+                            <option key={country.id} value={country.id}>{country.name}</option>
+                        ))}
+                    </select>
+                    <br />
+                    <span>{errors.country}</span>
+                </div>
 
                 <div>
                     {selectCountries?.map((select, index) => {
@@ -219,10 +469,9 @@ export default function Form() {
                                     <option value="">Selecciona un country--</option>
                                     {orderedCountries.map((country, index) => {
                                         return (
-                                            <option value={`${country.id}`}>{`${country.name}`}</option>
+                                            <option key={country.id} value={country.id}>{country.name}</option>
                                         )
-                                    })
-                                    }
+                                    })}
                                 </select>
                             </div>
                         )
--- client/src/components/Create/validations.js
+++ client/src/components/Create/validations.js
@@ -1,46 +1,72 @@
 
-
 export default function validations(propiedad, errors, setErrors, form) {
     switch (propiedad) {
         case "name":
             if (!form.name) {
-                setErrors({...errors, name: "Este campo es obligatorio"})
+                setErrors(prevErrors => ({...prevErrors, name: "Este campo es obligatorio"}));
             } else if(/^[A-Za-z\s]{3,30}$/.test(form.name)) {
-                setErrors({...errors, name:""})
+                setErrors(prevErrors => ({...prevErrors, name: ""}));
             } else {
-                setErrors({...errors, name: "El nombre solo puede estar conformado por letras. Y debe tener una longitud entre 3 a 30 caracteres"})
+                setErrors(prevErrors => ({...prevErrors, name: "El nombre solo puede estar conformado por letras y debe tener una longitud entre 3 a 30 caracteres"}));
             }
+            //regex para verificar mayusculas, minursculas y que tenga entre 3 y 30ch
             break;
         case "dificulty":
             if (!form.dificulty || form.dificulty === "seleccionar") {
-                setErrors({...errors, dificulty: "Este campo es obligatorio"})
+                setErrors(prevErrors => ({...prevErrors, dificulty: "Este campo es obligatorio"}));
             } else {
-                setErrors({...errors, dificulty:""})
+                setErrors(prevErrors => ({...prevErrors, dificulty: ""}));
             }
             break;
         case "season":
             if (!form.season) {
-                setErrors({...errors, season: "Este campo es obligatorio"})
+                setErrors(prevErrors => ({...prevErrors, season: "Este campo es obligatorio"}));
             } else {
-                setErrors({...errors, season:""})
+                setErrors(prevErrors => ({...prevErrors, season: ""}));
             }
             break;
         case "country":
             if (!form.country || form.country === "Selecciona un country--") {
-                setErrors({...errors, country: "Este campo es obligatorio"})
+                setErrors(prevErrors => ({...prevErrors, country: "Este campo es obligatorio"}));
             } else {
-                setErrors({...errors, country:"", countries: ""})
+                setErrors(prevErrors => ({...prevErrors, country: "", countries: ""}));
             }
             break;
         case "countries":
-            if (form[form.length-1].country === "") {
-                setErrors({...errors, countries: "Este campo es obligatorio"})
-                return
+            if (form.length > 0 && form[form.length - 1].country === "") {
+                setErrors(prevErrors => ({...prevErrors, countries: "Este campo es obligatorio"}));
             } else {
-                setErrors({...errors, countries:""})
+                setErrors(prevErrors => ({...prevErrors, countries: ""}));
             }
             break;
         default:
             break;
     }
+}
+
+export function validateForm(form, selectCountries, setErrors) {
+    const errors = {};
+
+    if (!form.name) {
+        errors.name = "Este campo es obligatorio";
+    }
+
+    if (!form.dificulty || form.dificulty === "seleccionar") {
+        errors.dificulty = "Este campo es obligatorio";
+    }
+
+    if (!form.season) {
+        errors.season = "Este campo es obligatorio";
+    }
+
+    if (!form.country || form.country === "Selecciona un país--") {
+        errors.country = "Este campo es obligatorio";
+    }
+
+    if (selectCountries.length > 0 && selectCountries[selectCountries.length - 1].country === "") {
+        errors.countries = "Este campo es obligatorio";
+    }
+
+    setErrors(errors);
+    return errors;
 }
